99638232c6641b7b6e33a4ffb19f35ccc95a9546
harrysudana <harrysudana@gmail.com>
2018-07-04 23:08:46 +0800

    add ButtonCompnent and ColorComponent in render

--- src/App.js
+++ src/App.js
@@ -16,6 +16,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+              <ButtonComponent/>
+              <ColorComponent/>
+        </div>
+
       </div>
     );
   }
