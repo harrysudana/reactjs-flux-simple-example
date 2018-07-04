db03a8200ff211842e521f9690750635cf9cb0f7
harrysudana <harrysudana@gmail.com>
2018-07-05 07:06:39 +0800

    add stores named ColorAppStore

---
+++ src/stores/ColorAppStore.js
@@ -0,0 +1,32 @@
+import {EventEmitter} from "events";
+import dispatcher from "../Dispatcher";
+import AppActionTypes from '../ActionTypes';
+import ColorAppActions from "../actions/ColorAppActions";
+
+class ColorAppStore extends EventEmitter {
+
+    constructor() {
+        super();
+        this.activeColor = "lightgrey";
+    }
+
+    handleActions(action) {
+        switch (action.type) {
+            case ColorAppActions.AppActionTypes.CHANGE_COLOR: {
+                this.activeColor = action.value;
+                this.emit("storeUpdated");
+                break;
+            }
+            default: {
+            }
+        }
+    }
+
+    getActiveColor() {
+        return this.activeColor;
+    }
+}
+
+const colorAppStore = new ColorAppStore();
+dispatcher.register(colorAppStore.handleActions.bind(colorAppStore));
+export default colorAppStore;
